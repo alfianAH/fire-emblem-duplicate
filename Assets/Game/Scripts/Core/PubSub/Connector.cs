a74e6e23b5583fa6186a05ad8bb4afb3fa49c2c0
Alfian Aldy Hamdani <alfian.hamdani04@gmail.com>
2024-02-02 16:48:15 +0800

    Add controller generic class in connector

--- Assets/Game/Scripts/Core/PubSub/Connector.cs
+++ Assets/Game/Scripts/Core/PubSub/Connector.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 
 namespace FireEmblemDuplicate.Core.PubSub
 {
-    public abstract class Connector<T> : MonoBehaviour
+    public abstract class Connector<T, C> : MonoBehaviour
         where T : MonoBehaviour
+        where C : MonoBehaviour
     {
+        protected C controller;
+
         public abstract void Subscribe();
 
         public abstract void Unsubscribe();
+
+        private void Awake()
+        {
+            controller = GetComponent<C>();
+        }
 
         private void OnEnable()
         {
--- Assets/Game/Scripts/Scene/Battle/Unit/BaseUnitConnector.cs
+++ Assets/Game/Scripts/Scene/Battle/Unit/BaseUnitConnector.cs
@@ -4,27 +4,20 @@ using SuperMaxim.Messaging;
 
 namespace FireEmblemDuplicate.Scene.Battle.Unit
 {
-    public class BaseUnitConnector : Connector<BaseUnitConnector>
+    public class BaseUnitConnector : Connector<BaseUnitConnector, BaseUnitController>
     {
-        private BaseUnitController _controller;
-
-        private void Awake()
-        {
-            _controller = GetComponent<BaseUnitController>();
-        }
-
         public override void Subscribe()
         {
-            Messenger.Default.Subscribe<OnClickUnit>(_controller.OnUnitClick);
-            Messenger.Default.Subscribe<OnStartDragUnit>(_controller.OnStartDragUnit);
-            Messenger.Default.Subscribe<OnEndDragUnit>(_controller.OnEndDragUnit);
+            Messenger.Default.Subscribe<OnClickUnit>(controller.OnUnitClick);
+            Messenger.Default.Subscribe<OnStartDragUnit>(controller.OnStartDragUnit);
+            Messenger.Default.Subscribe<OnEndDragUnit>(controller.OnEndDragUnit);
         }
 
         public override void Unsubscribe()
         {
-            Messenger.Default.Unsubscribe<OnClickUnit>(_controller.OnUnitClick);
-            Messenger.Default.Unsubscribe<OnStartDragUnit>(_controller.OnStartDragUnit);
-            Messenger.Default.Unsubscribe<OnEndDragUnit>(_controller.OnEndDragUnit);
+            Messenger.Default.Unsubscribe<OnClickUnit>(controller.OnUnitClick);
+            Messenger.Default.Unsubscribe<OnStartDragUnit>(controller.OnStartDragUnit);
+            Messenger.Default.Unsubscribe<OnEndDragUnit>(controller.OnEndDragUnit);
         }
     }
 }
